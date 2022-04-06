888b936fab23309331ed5d447c8dfb1e4ac42ed9
hafizsheetab <muhtaseen@iut-dhaka.edu>
2022-04-07 02:28:45 +0600

    Update Bug Fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require('path');
-app.use(express.static("client/build"));
+app.use(express.static("prototypefrontend/build"));
 app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "prototypefrontend", "build", "index.html"));
 });
